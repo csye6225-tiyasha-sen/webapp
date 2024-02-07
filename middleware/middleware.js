4ee70fea4f490c0f55ca36bc1b918995e1f4797a
Tiyasha Sen <sen.ti@northeastern.edu>
2024-02-07 17:55:25 -0500

    Updated the validations for handling 400 status code

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,11 +1,22 @@
-const routesAllowed = ["/healthz", "/v1/user", "/v1/user/self"];
+//const routesAllowed = ["/healthz", "/v1/user", "/v1/user/self"];
 
-export const checkRoutes = (req, res, next) => {
-  if (!routesAllowed.includes(req.path)) {
-    return res.status(404).end();
-  }
-  return next();
+const routesAllowed = {
+  "/healthz": ["GET"],
+  "/v1/user": ["POST"],
+  "/v1/user/self": ["PUT", "GET"],
 };
+// export const checkRoutes = (req, res, next) => {
+//   const allowedMethods = routesAllowed[req.path];
+//   if (!allowedMethods) {
+//     return res.status(404).end();
+//   }
+//   return next();
+// };
+
+// if (!routesAllowed(req.path).includes(req.method)) {
+//   return res.status(404).end();
+// }
+// return next();
 
 export const checkReqMethods = (req, res, next) => {
   //console.log(`${req.method}`);
@@ -14,4 +25,21 @@ export const checkReqMethods = (req, res, next) => {
   } else {
     return res.status(405).send();
   }
+};
+
+export const checkReqMethodsForUser = (req, res, next) => {
+  //console.log(`${req.method}`);
+  const allowedMethods = routesAllowed[req.path];
+  console.log(`Request path: ${req.path}`);
+
+  if (!allowedMethods) {
+    console.log(`Request method: ${req.method}`);
+    return res.status(404).send();
+  }
+
+  if (!allowedMethods.includes(req.method)) {
+    return res.status(405).send();
+  }
+
+  return next();
 };
