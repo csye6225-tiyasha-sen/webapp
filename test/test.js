a61f64216f4c56e87f0f90a5a02998021fe59ac0
Tiyasha Sen <113063005+TiyashaSen@users.noreply.github.com>
2024-02-12 18:04:43 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ import { expect } from "chai";
 
 describe("Test cases running", () => {
   it("Route test cases included", (done) => {
-    expect(1 + 1).to.equal(2);
+    expect(1 + 2).to.equal(2);
     done();
   });
 });
