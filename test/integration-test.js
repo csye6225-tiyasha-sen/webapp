8c01fdcdbc8efe617f5d691591a0fbb004db2bcc
Tiyasha Sen <sen.ti@northeastern.edu>
2024-02-13 12:11:03 -0500

    Add the first integration test scenario

---
+++ test/integration-test.js
@@ -0,0 +1,37 @@
+import express from "express";
+import request from "supertest";
+import app from "../app.js";
+
+before((done) => {
+  app.listen(4000);
+  done();
+});
+
+describe("Validate if account exists", function () {
+  // Test to create an account
+  it("POST /v1/user - Create an account and respond with JSON", function (done) {
+    request(app)
+      .post("/v1/user")
+      .send({
+        first_name: "test",
+        last_name: "Sen",
+        password: "Tiyasha123",
+        username: "sen.testtt@northeastern.edu",
+      })
+      .set("Accept", "application/json")
+      .set("Content-Type", "application/json")
+      //.expect("Content-Type", "text/html; charset=utf-8")
+      .expect(201)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  it("GET /users/:id - Validate the created account exists", function (done) {
+    request(app)
+      .get("/v1/user/self")
+      .auth("sen.li@northeastern.edu", "Tiyasha123")
+      .expect(200, done);
+  });
+});
