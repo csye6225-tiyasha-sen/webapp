30e9a1fe984d4ebd2bc78083b567e883e9631c4c
Tiyasha Sen <sen.ti@northeastern.edu>
2024-02-21 15:52:04 -0500

    Added echo message

--- scripts/db.sh
+++ scripts/db.sh
@@ -21,9 +21,8 @@ sudo systemctl restart postgresql
 
 
 # Load environment variables from .env file
-
+echo "Alter: USER:$PSQL_USER PASSWORD:$PSQL_PASSWORD DB:$PSQL_DATABASE"
 #alter script
 sudo -u postgres psql -c "ALTER USER $PSQL_USER PASSWORD '$PSQL_PASSWORD';"
 # Create the database
 sudo -u postgres psql -c "CREATE DATABASE $PSQL_DATABASE;"
-echo "Database created successfully."
--- scripts/load.sh
+++ scripts/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# Yum install
 sudo yum install unzip -y
