aead91c1d1c77fa600ae8c154eb11d593815a662
Tiyasha Sen <sen.ti@northeastern.edu>
2024-02-21 16:25:19 -0500

    Updated secrets section

--- scripts/systemd.sh
+++ scripts/systemd.sh
@@ -1,6 +1,6 @@
 sudo adduser csye6225 --shell /usr/sbin/nologin
 sudo cp -r /tmp/webappdev.service /etc/systemd/system/
-sudo chown -R csye6225:csye6225 /etc/systemd/system/webappdev.service
+sudo chown -R csye6225:csye6225 /home/tiyashasen_net/webapp
 
 sudo sed -i "s/enforcing/disabled/g" /etc/selinux/config
 sudo setenforce 0
