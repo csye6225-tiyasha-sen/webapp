4a48d79f906f4ef43362538db22cbfae6d6b751d
Tiyasha Sen <sen.ti@northeastern.edu>
2024-02-21 11:59:51 -0500

    Set enforce to original

--- scripts/systemd.sh
+++ scripts/systemd.sh
@@ -2,7 +2,8 @@ sudo adduser csye6225 --shell /usr/sbin/nologin
 sudo cp -r /tmp/webappdev.service /etc/systemd/system/
 sudo chown -R csye6225:csye6225 /etc/systemd/system/webappdev.service
 
-
+sudo sed -i "s/enforcing/disabled/g" /etc/selinux/config
+sudo setenforce 0
 sudo systemctl daemon-reload
 
 # Enable the service to start on boot
