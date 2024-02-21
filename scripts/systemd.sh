aeaf81f65e61c4aab7698d5e711f110ea3871308
Tiyasha Sen <sen.ti@northeastern.edu>
2024-02-20 23:28:25 -0500

    Changed comment in systemd file

--- scripts/systemd.sh
+++ scripts/systemd.sh
@@ -4,7 +4,6 @@ sudo chown -R csye6225:csye6225 /etc/systemd/system/webappdev.service
 
 sudo sed -i "s/enforcing/disabled/g" /etc/selinux/config
 sudo setenforce 0
-# Reload systemd to pick up the new service file
 sudo systemctl daemon-reload
 
 # Enable the service to start on boot
