606f1d35128a716acb35c41f51605f4e9c746de0
Tiyasha Sen <sen.ti@northeastern.edu>
2024-02-21 19:35:00 -0500

    Updated zipunzip

--- scripts/zipunzip.sh
+++ scripts/zipunzip.sh
@@ -3,5 +3,13 @@
 unzip /tmp/webapp.zip -d ~/
 cd ~/webapp
 
+touch .env
+
+cat > .env <<EOF
+PSQL_USERNAME: ${PSQL_USERNAME}
+PSQL_PASSWORD: ${PSQL_PASSWORD}
+PSQL_DATABASE: ${PSQL_DATABASE}
+EOF
+
 npm install
 
