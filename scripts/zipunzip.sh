db4c63f9deb8a9aece910ac9dc2493bd56dac20c
Tiyasha Sen <sen.ti@northeastern.edu>
2024-02-26 20:12:43 -0500

    Updated zipunzip for sudo changes

--- scripts/zipunzip.sh
+++ scripts/zipunzip.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo mkdir /opt/csye6225
-unzip /tmp/webapp.zip -d /opt/csye6225/
+sudo chown -R $(whoami) /opt/csye6225
+sudo unzip /tmp/webapp.zip -d /opt/csye6225/
 cd /opt/csye6225/webapp && sudo npm install
 
 
