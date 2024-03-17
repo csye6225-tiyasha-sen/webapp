fc309b393df937ca925ca80eac962933f3cefaef
Tiyasha Sen <sen.ti@northeastern.edu>
2024-03-17 19:45:28 -0400

    Updated the permissions of folder /var/log

--- scripts/installopsagent.sh
+++ scripts/installopsagent.sh
@@ -2,6 +2,9 @@
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
+sudo mkdir -p /var/log/webapp/
+sudo touch /var/log/webapp/myapp.log
+
 #Ops Agent configuration
 CONFIG_CONTENT="
 logging:
@@ -38,3 +41,4 @@ echo "$CONFIG_CONTENT" | sudo tee "$CONFIG_FILE" > /dev/null
 
 #Restart
 sudo systemctl restart google-cloud-ops-agent
+
--- scripts/systemd.sh
+++ scripts/systemd.sh
@@ -1,10 +1,11 @@
 sudo adduser csye6225 --shell /usr/sbin/nologin
 sudo cp -r /tmp/webappdev.service /etc/systemd/system/
 sudo chown -R csye6225:csye6225 /opt/csye6225/webapp
-
+sudo chown -R csye6225:csye6225 /var/log/webapp
 # sudo sed -i "s/enforcing/disabled/g" /etc/selinux/config
 sudo setenforce 0
 sudo systemctl daemon-reload
 
 # Last step: Enable the service to start on boot
+sudo systemctl restart google-cloud-ops-agent
 sudo systemctl enable webappdev.service
