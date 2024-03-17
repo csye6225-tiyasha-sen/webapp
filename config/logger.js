01ae22ca792b772011dba9c30c6a2d443a7151f8
Tiyasha Sen <sen.ti@northeastern.edu>
2024-03-17 15:08:30 -0400

    Updated changes for Ops Agent config and logger configuration

---
+++ config/logger.js
@@ -0,0 +1,29 @@
+import winston from "winston";
+import { createLogger, format, transports } from "winston";
+import appRoot from "app-root-path";
+
+const { combine, timestamp, label, printf } = format;
+const CATEGORY = "logger";
+
+const customFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  level: "info",
+  format: combine(
+    label({ label: CATEGORY }),
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    customFormat
+  ),
+  transports: [
+    new winston.transports.File({
+      filename: appRoot + "/logs/csye6225.log",
+    }),
+    new transports.Console(),
+  ],
+});
+
+export default logger;
