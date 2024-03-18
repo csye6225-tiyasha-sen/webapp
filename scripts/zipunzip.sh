de2a79ad2090e372e47936932e1de2c4d3127e1f
Tiyasha Sen <sen.ti@northeastern.edu>
2024-03-17 23:48:49 -0400

    Updated the permission changes

--- scripts/zipunzip.sh
+++ scripts/zipunzip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo mkdir -p /var/log/webapp/
+sudo mkdir -p /var/log/webapp
 sudo mkdir /opt/csye6225
 sudo unzip /tmp/webapp.zip -d /opt/csye6225/
 cd /opt/csye6225/webapp && sudo npm install
