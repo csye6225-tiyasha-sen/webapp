e12e65134f7269ff06e95f706a54294643b3bb58
Tiyasha Sen <sen.ti@northeastern.edu>
2024-03-17 23:40:06 -0400

    Updated the permission changes

--- scripts/installopsagent.sh
+++ scripts/installopsagent.sh
@@ -2,8 +2,6 @@
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
-sudo mkdir -p /var/log/webapp/
-sudo touch /var/log/webapp/myapp.log
 #Ops Agent configuration
 CONFIG_CONTENT="
 logging:
