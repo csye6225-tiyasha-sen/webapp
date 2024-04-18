8c0100bdc39830305cccd2f8ea06b84cab801b7e
Tiyasha Sen <sen.ti@northeastern.edu>
2024-04-18 16:37:41 -0400

    updated from v1 to v6

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -3,8 +3,8 @@ import db from "../config/dbConfig.js";
 
 const routesAllowed = {
   "/healthz": ["GET"],
-  "/v1/user": ["POST"],
-  "/v1/user/self": ["PUT", "GET"],
+  "/v6/user": ["POST"],
+  "/v6/user/self": ["PUT", "GET"],
   "/verifyUser": ["GET"],
 };
 
